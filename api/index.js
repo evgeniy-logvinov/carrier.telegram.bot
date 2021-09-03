46cad683565a815bf6b4d2eb82fb354e75494bfd
Evgeniy Logvinov <evgeniy.logvinov.k@gmail.com>
2021-09-03 15:33:15 +0300

    token from env

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const app = require('express')();
 const { v4 } = require('uuid');
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '1992554316:AAF7c-3RBRUlSjRc5trjk9IZP4DOlfB8Yac';
+const token = process.env.BOT_TOKEN;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
